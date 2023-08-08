f6f85a7e7442cb3dae4d0887cf75b78d8c5f664a
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-08 23:42:08 +0100

    Alx

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j), end="")
-        else:
-            print("{}{}, ".format(i, j), end="")
+for i in range(100):
+    if i != 99:
+        print("{}, ".format(i), end="")
+    else:
+        print("{}".format(i))
