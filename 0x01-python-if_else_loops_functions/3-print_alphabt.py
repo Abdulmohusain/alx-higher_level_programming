0cce0703e65df436461e594809f26767306d2fd4
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-08 12:47:46 -0700

    Alx

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(65, 91):
-         print("{:c}".format(i), end="")
+for i in range(97, 123):
+    if ord('e') == i or i == ord('e'):
+        continue
+    print("{:c}".format(i), end="")
