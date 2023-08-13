72895778ddbd66db4d1870b21b061caae015b2de
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-13 19:50:31 +0100

    Alx

---
+++ 0x02-python-import_modules/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(a) < 2:
+        return (a[0] + b[0]), b[0]
+    if len(b) < 2:
+        return b[0], (a[0] + b[0])
+    elif len(a) >+ 2:
+
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+
