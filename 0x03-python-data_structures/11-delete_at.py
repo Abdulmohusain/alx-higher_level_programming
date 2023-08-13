1e85e5c887adbd1d67be53acc93fda93cb0ff06b
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-13 14:26:55 +0100

    Alx

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    
+def delete_at(my_list=[], idx=0):
+    if type(my_list) is list or idx < len(list):
+        del (my_list[idx])
+    return my_list
