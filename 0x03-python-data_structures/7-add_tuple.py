8e856f9170f046d8c6526da834d5a791c393cca2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-13 13:31:45 -0700

    Alx

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        return tuple_b[0], tuple_b[1]
-    if len(tuple_b) == 0:
-        return tuple_a[0], tuple_a[1]
-    if len(tuple_a) == 1 and len(tuple_b) == 1:
-        return tuple_a[0], tuple_b[0]
-    if len(tuple_a) == 2 and len(tuple_b) == 1:
-        return (tuple_a[0] + tuple_b[0]), tuple_a[1]
-    if len(tuple_a) == 1 and len(tuple_b) == 2:
-        return (tuple_a[0] + tuple_b[0]), tuple_b[1]
-    return (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
+    if type(tuple_a) is tuple and  type(tuple_b) is tuple:
+        if len(tuple_a) == 0:
+            return tuple_b[0], tuple_b[1]
+        if len(tuple_b) == 0:
+            return tuple_a[0], tuple_a[1]
+        if len(tuple_a) == 1 and len(tuple_b) == 1:
+            return tuple_a[0], tuple_b[0]
+        if len(tuple_a) == 2 and len(tuple_b) == 1:
+            return (tuple_a[0] + tuple_b[0]), tuple_a[1]
+        if len(tuple_a) == 1 and len(tuple_b) == 2:
+            return (tuple_a[0] + tuple_b[0]), tuple_b[1]
+        return (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
