38a5ae0e9f1673919ea2f76c3934b141adce2fac
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-13 19:56:01 +0100

    Alx

--- 0x02-python-import_modules/7-add_tuple.py
+++ 0x02-python-import_modules/7-add_tuple.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(a) < 2:
-        return (a[0] + b[0]), b[0]
-    if len(b) < 2:
-        return b[0], (a[0] + b[0])
-    elif len(a) >+ 2:
+    if type(tuple_a) is tuple and type(tuple_b) is tuple:
+        if len(a) < 1 or len(b) < 1:
+
+        
+        if len(a) < 2 or len(b) < 2:
+            return a[0], b[0]
+        if len(a) < 2:
+            return (a[0] + b[0]), b[0]
+        if len(b) < 2:
+            return b[0], (a[0] + b[0])
+        elif len(a) >+ 2:
 
 
 
