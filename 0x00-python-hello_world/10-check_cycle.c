c99fad0775e7b7c6a8d2a8d0c96bb1cd278f74db
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-15 00:14:54 +0100

    Alx

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,10 +8,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *start = list;
 
-	if (list == NULL)
-		return (0)
-	list = list->next;
-	while (list)
+	while (list->next)
 	{
 		if (list == start)
 			return (1);
