7714aeddecb48d95afa876ee5eb8d510d0fe2f77
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-14 11:42:13 -0700

    Alx

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a is None or tuple_b is None:
+        return
     if type(tuple_a) is tuple and type(tuple_b) is tuple:
         if len(tuple_a) >= 2:
             if len(tuple_b) >= 2:
