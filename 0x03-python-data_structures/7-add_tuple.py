1eb6d47375d29ac825ff4fc21f18d4ffcf2bb3d2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-14 10:23:47 -0700

    Alx

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -22,3 +22,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 return (tuple_b[0])
             if len(tuple_b) == 0:
                 return
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple((1), (1, 2)))
+print(add_tuple((1), (1)))
+print(add_tuple((1) ))
