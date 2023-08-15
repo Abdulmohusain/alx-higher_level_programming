75cb28402a131fc9e47f1ef06f2a794d3fe95b64
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-15 02:44:20 -0700

    Alx

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sy as sys
+    import sys as sy
     import calculator_1 as cal
 
     arg = sy.argv
     if len(arg) != 4:
-        print("Usage: {:s} <a> <operator> <b>".format(arg[0]))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     op_list = ["+", "-", "*", "/"]
     a = int(arg[1])
@@ -15,10 +15,10 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     if o == "+":
-        print("{:d}".format(cal.add(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, o, b, cal.add(a, b)))
     if o == "-":
-        print("{:d}".format(cal.sub(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, o, b, cal.sub(a, b)))
     if o == "*":
-        print("{:d}".format(cal.mul(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, o, b, cal.mul(a, b)))
     if o == "/":
-        print("{:d}".format(cal.div(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, o, b, cal.div(a, b)))
