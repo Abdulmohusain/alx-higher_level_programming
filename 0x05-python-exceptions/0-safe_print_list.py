cd82c93f7f6efd91c9ad1eac144305b8e7114d5d
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-28 22:32:49 +0100

    Alx

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    if type(my_list) != list:
+    if type(my_list) is not list:
         return 0
     j = 0
     k = 0
@@ -19,20 +18,3 @@ def safe_print_list(my_list=[], x=0):
                 break
     print("")
     return j
-
-
-
-
-
-
-
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
