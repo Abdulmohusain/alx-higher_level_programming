df220ad59c5fe1e88c80f4cfd64f4ca5ae29064d
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-29 00:15:47 +0100

    Alx

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if type(my_list) is not list:
+        return 0
+    j = 0
+    k = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except TypeError:
+            continue
+        except IndexError:
+            continue
+        except ValueError:
+            continue
+        finally:
+            k += 1
+            if k == x:
+                break
+    print("")
+    return j
