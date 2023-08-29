e4d2cb1e09100e9dc297e555c4b91ea9ca9c0487
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-29 01:32:03 +0100

    Alx

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,10 +8,7 @@ def safe_print_division(a, b):
         result = None
     finally:
         if result:
-            try:
-                print("Inside result: {:d}".format(result))
-            except ValueError:
-                print("Inside result: {:.1f}".format(result))
+            print("Inside result: {:.1f}".format(result))
         else:
             print("Inside result: None")
         return result
