4b9f08e597766fbb02fd8c6e892b8e2b0a8bd118
Abdullahi Husain <abdulmohusain@gmail.com>
2023-08-29 01:38:47 +0100

    Alx

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,6 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        if result:
-            print("Inside result: {:.1f}".format(result))
+        print("Inside result: {:.1f}".format(result))
         return result
