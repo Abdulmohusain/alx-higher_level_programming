7989fb0a465fb1228812a9dad8a5340a01399ed7
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-01 07:48:22 -0700

    Alx

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+class Square:
+    """this module describes a class square"""
+    def __init__(self, size):
+        self.__size  = size
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+        print(my_square.size)
+except Exception as e:
+        print(e)
+
+        try:
+                print(my_square.__size)
+        except Exception as e:
+                print(e)
