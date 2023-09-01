226aad2d0659bd18120e119673036b9439b00ef7
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-01 17:08:35 +0100

    Alx

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
+"""This module creates a Square class"""
+
+
 class Square:
+    """This create the class square"""
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+    def __init__(self, size=0):
+        """All attributes are instantiated
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+        Args:
+            size (int): Length of square
+        """
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
