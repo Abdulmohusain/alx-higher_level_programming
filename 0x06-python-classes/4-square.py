b9940fdbe1b2e5e93c3e1a0bdad779b0e3ac5a52
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-01 17:52:32 +0100

    Alx

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,6 +26,10 @@ class Square:
     @size.setter
     def size(self, value):
         """Sets the private attribute __size to value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
