7db513450d7df94e8c57fc09fa09b6a49c240c14
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-04 20:18:16 +0100

    Alx

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
 
     def __repr__(self):
         """returns “official” string representation of an object."""
-        return 'Rectangle({}, {})'.format(self.__width, self.__height)
+        return 'Rectangle({:d}, {:d})'.format(self.__width, self.__height)
 
     @property
     def width(self):
