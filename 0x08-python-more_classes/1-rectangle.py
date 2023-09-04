4a2b2ad95b159a16fe9499288e9cd9410bcbe542
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-04 12:11:50 +0100

    Alx

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Module for class Rectangle"""
+
+
+class Rectangle:
+    """Creates a rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the class"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Return private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set private instance attribute width"""
+        if type(self.__width) is not int:
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set private instance attribute height"""
+        if type(self.__height) is not int:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
