2286e2014a1581e474bc3c77090c1902fa5660df
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-12 18:10:07 -0400

    Alx

--- 0x01-python-if_else_loops_functions/13-main.c
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -26,6 +26,9 @@ int main(void)
     printf("-----------------\n");
 
     insert_node(&head, 27);
+    insert_node(&head, 1);
+    insert_node(&head, 0);
+    insert_node(&head, 1043);
 
     print_listint(head);
 
