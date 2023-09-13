3cb7b72960278a19245e3a533e629178108b9954
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-13 13:05:17 -0400

    Alx

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Search replace """
+
+
+def search_replace(my_list, search, replace):
+    """Search replace"""
+    if type(my_list) is not list:
+        return
+    list_copy = my_list[:]
+    for i, j in enumerate(list_copy):
+        if j == search:
+            list_copy.remove(j)
+            list_copy.insert(i, replace)
+            return list_copy
+    return list_copy
