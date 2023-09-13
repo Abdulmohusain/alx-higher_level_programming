cb4fe1b8511884b9f141ef3d88bb79a94615ee94
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-13 15:03:08 -0700

    Alx

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-"""function that deletes a key in a dictionary."""
-
 def multiply_list_map(my_list=[], number=0):
-    """returns a list with all values multiplied
-    by a number"""
-    if type(my_list) is not list:
-        return
-    if len(my_list) == 0:
-        return
-    return [i * 2 for i in my_list[:]]
+    return list(map(lambda x: x * 2, my_list))
