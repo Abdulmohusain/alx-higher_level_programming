58bec7ff77ad61ad4831ed943e84af756e31a05a
Abdullahi Husain <abdulmohusain@gmail.com>
2023-09-13 14:11:47 -0400

    Alx

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Sorted dict"""
+
+def print_sorted_dictionary(a_dictionary):
+    """Print sorted dict"""
+    if type(a_dictionary) is not dict:
+        return
+    for key, value in sorted(a_dictionary.items()):
+        print("{:s}: {}".format(key, value))
