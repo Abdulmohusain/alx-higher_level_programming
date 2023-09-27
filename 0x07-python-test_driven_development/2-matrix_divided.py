b13dee963c410b390e7ad07a543dfa3a0e124a4d
Abdullahi Husain <abdulmohusain>
2023-09-27 13:31:22 +0100

    Alx

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -35,6 +35,7 @@ def matrix_divided(matrix, div):
             if type(num) is not int and type(num) is not float:
                 raise TypeError("matrix must be a matrix (list of lists)\
                     of integers/floats")
+        new = matrix[:]
         return list(map(
             lambda x: list(map(
-                lambda y: round(y / 3, 2), x)), matrix))
+                lambda y: round(y / 3, 2), x)), new))
