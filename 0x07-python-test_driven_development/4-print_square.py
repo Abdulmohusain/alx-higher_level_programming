03b34e295f431110c1a248dd5b8269f1ed306427
Abdullahi Husain <abdulmohusain>
2023-09-27 22:56:42 +0100

    ALX

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to divide matrix by 3
+Args:
+    first_name: string
+    last_name: string
+"""
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
