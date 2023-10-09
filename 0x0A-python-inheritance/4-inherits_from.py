486839e6602534db6178d3df2ab1544952643dd2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-09 16:52:27 +0100

    Alx

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,16 @@ def inherits_from(obj, a_class):
         a_class: the class
 
     """
-    return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
+
+    
+
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
