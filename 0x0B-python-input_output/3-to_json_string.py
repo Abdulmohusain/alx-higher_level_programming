3ef204ab447899d3a0fbe1d249998d629c1f2199
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-11 00:30:38 +0100

    Alx

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module contains to_json_string function"""
+import json
+
+
+def to_json_string(my_obj):
+    """a function that returns a json class"""
+    return json.dumps(my_obj)
