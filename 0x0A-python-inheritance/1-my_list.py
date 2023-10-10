5538b53389913d7fc8209ef9d7143193fbf28fb2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-10 10:28:28 +0100

    Alx

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python3
+"""This module contains class my_list with
+    method to sort the list
+"""
+
+
 class MyList(list):
+    """This defines n new list class that inherits from
+        list
+    """
     def print_sorted(self):
-        pass
+        """Function that prints sorted list"""
+        for i in self:
+            if type(i) is not int:
+                raise TypeError("list must contain only integers")
+        print(sorted(self))
