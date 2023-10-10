1ef2ab1649a7e1c562c3111c59fc496459e3ee05
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-10 11:53:52 +0100

    Alx

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""this module contains Square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """this defines Square class that inherits from BaseGeometry"""
+    def __init__(self, size):
+        """The initialization method"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Return a nice printable format of object"""
+        return "[{}] {}/{}".format("Square", self.__size, self.__size)
+
+    def area(self):
+        """computes the area"""
+        return self.__size * self.__size
