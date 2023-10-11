6a5670e5ec0946bcfb9f6effb1a3e1181caef944
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-11 11:42:00 +0100

    Alx

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""this module contains from_json_string function"""
+import json
+
+
+def class_to_json(obj):
+    """function that returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object:
+    Args:
+        obj (object): is an instance of a Class
+    """
+    name, number = str(obj).replace("[MyClass] ", '').replace("-", "").split()
+
+    new_dict = {}
+    new_dict["name"] = name
+    new_dict["number"] = number
+    return json.dumps(new_dict)
