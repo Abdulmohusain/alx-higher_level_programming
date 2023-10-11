3d02a5e57381685cedb461ead96f1d7f7c882f7a
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-11 10:45:55 +0100

    Alx

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""this module contains from_json_string function"""
+import json
+
+
+def load_from_json_file(filename):
+    """a function that creates an Object to a text
+    file, using a JSON representation:
+    Args:
+        filename (str): the file name
+    """
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
