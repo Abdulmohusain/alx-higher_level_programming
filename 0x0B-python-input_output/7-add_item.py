5f7062cd652b2988b418088999a741d57aedca2e
Abdullahi Husain <abdulmohusain@gmail.com>
2023-10-11 11:18:57 +0100

    Alx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""script that adds all arguments to a Python list,
+and then save them to a file:"""
+import sys
+import json
+
+
+def main():
+    lis = []
+    for arg in sys.argv[1:]:
+        lis.append(arg)
+    try:
+        with open("add_item.json", 'r') as f:
+            old_list = json.load(f)
+        new_list = old_list + lis
+
+        with open("add_item.json", 'w') as f:
+            json.dump(new_list, f)
+
+    except FileNotFoundError:
+        with open("add_item.json", 'w') as f:
+            json.dump(lis, f)
+
+
+if __name__ == "__main__":
+    main()
