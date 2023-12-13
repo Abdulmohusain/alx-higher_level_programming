1f13fc73b003e889bd3c629c03210c2c251873a5
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-13 11:24:03 +0100

    Alx

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv[2] === undefined) {
+const num = parseInt(argv[2]);
+if (isNaN(argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const num = parseInt(argv[2]);
-  if (isNaN(argv[2])) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < num; i++) {
-      console.log('C is fun');
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
   }
 }
