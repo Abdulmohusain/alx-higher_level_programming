169c06c4ddaa114093d914399bbf2cffce83bf98
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-13 12:06:55 +0100

    Alx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const argv = process.argv;
+let highest = 0;
+let secondHighest = 0;
+if (argv.length < 4) {
+  console.log(secondHighest);
+} else {
+  for (let i = 2; i < argv.length; i++) {
+    const num = parseInt(argv[i]);
+    if (num > highest) {
+      secondHighest = highest;
+      highest = num;
+    } else if (num < highest) {
+      if (num > secondHighest) {
+        secondHighest = num;
+      }
+    }
+  }
+  console.log(secondHighest);
+}
