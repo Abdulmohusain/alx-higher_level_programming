d06ef00a60f766dcbdef463ffa8be29618145bae
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-13 11:21:10 +0100

    Alx

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-for (let i = 0; i < 3; i++) {
-  switch (i) {
-    case 0:
-      console.log('C is fun');
-      break;
-    case 1:
-      console.log('Python is cool');
-      break;
-    case 2:
-      console.log('JavaScript is amazing');
-      break;
-    default:
-      break;
-  }
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
