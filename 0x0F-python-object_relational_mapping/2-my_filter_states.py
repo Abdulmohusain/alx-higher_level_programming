ab0b1dbf7ac6920b36a6e4c42d93553630fd7a47
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-16 20:24:55 +0100

    Alx

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,12 +10,12 @@ def main():
         host='localhost',
         port=3306,
         user=sys.argv[1],
-        password=sys.argv[2],
+        password='qwerty',
         database=sys.argv[3]
         )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name='{}' "
+        "SELECT * FROM states WHERE name='{:s}' "
         "ORDER BY states.id ASC;".format(sys.argv[4])
         )
     rows = cur.fetchall()
