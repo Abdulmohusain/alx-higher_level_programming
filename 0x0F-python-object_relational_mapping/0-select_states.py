a87ff4573c97a0ab4375b1e5066ed6f708ebd194
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-16 10:34:58 +0100

    Alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,14 @@ import sys
 if __name__ == '__main__':
     """Main"""
     if len(sys.argv) == 4:
-        db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], password="qwerty", database=sys.argv[3])
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+            )
+
         cur = db.cursor()
         cur.execute("USE hbtn_0e_0_usa;")
         cur.execute("SELECT * FROM states;")
