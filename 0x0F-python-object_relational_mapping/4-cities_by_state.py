1e13397364cd5e8b6459678b012659518625ce5a
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-16 21:26:06 +0100

    Alx

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module contains """
+import MySQLdb
+import sys
+
+
+def main():
+    """Main"""
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY cities.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    """Main"""
+    main()
