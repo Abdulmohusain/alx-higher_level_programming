17805e075e0577c3d36e16676818f7b522de6de2
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-16 16:37:34 +0100

    Alx

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ def main():
         host='localhost',
         port=3306,
         user=sys.argv[1],
-        password='qwerty',
+        password=sys.argv[2],
         database=sys.argv[3]
         )
 
