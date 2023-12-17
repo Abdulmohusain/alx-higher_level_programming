20f28aef60d9836e59fdf555c2c87918bac067d5
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-17 22:56:48 +0100

    Alx

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module contains main"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """Main"""
+    database_url = "mysql://{}:{}@localhost:3306/{}".format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]
+        )
+    engine = create_engine(database_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).filter(State.name.like('%A%')).all()
+    if row is not None:
+        print("{}: {}".format(row.id, row.name))
+    else:
+        print("Nothing")
+    session.close()
+
+
+if __name__ == '__main__':
+    """Main"""
+    main()
