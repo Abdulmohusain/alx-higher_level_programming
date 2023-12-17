af347de8a55c6ebb9efe3ec13619b49ee88132b6
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-17 16:29:54 +0100

    Alx

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module contains state class"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class
+    Attributes:
+        __tablename__ (str): Name of table
+        id (int): state_id which is the primary key
+        name (str): Name of state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
