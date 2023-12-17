565f318a616c3f1fad2e5693e78465a7d1030919
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-18 00:52:29 +0100

    Alx

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module contains main"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def main():
+    """Main"""
+    database_url = "mysql://{}:{}@localhost:3306/{}".format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]
+        )
+    engine = create_engine(database_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
+    session.commit()
+    # if state is not None:
+    #     print("{}: {}".format(state.id, state.name))
+    # else:
+    #     print("Nothing")
+    session.close()
+
+
+if __name__ == '__main__':
+    """Main"""
+    main()
