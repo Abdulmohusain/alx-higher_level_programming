17c442dbdfea755a36977e7895e67ee3fa3cd851
Abdullahi Husain <abdulmohusain@gmail.com>
2023-12-18 01:56:36 +0100

    Alx

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """Module contains state class"""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """State class
     Attributes:
         __tablename__ (str): Name of table
         id (int): state_id which is the primary key
         name (str): Name of state
+        state_id (int): state_id foreign key
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
+    state_id = Column(Integer, ForeignKey('states.id'))
