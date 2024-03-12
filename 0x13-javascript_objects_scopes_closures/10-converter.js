122514147e154a1711341cd00f3322ba45ffc12a
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-13 00:53:32 +0100

    converter

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (num) {
-    if (base === 10) {
-      return (base * (Math.floor(num / base)) + (num % base));
-    } else {
-      return (base * (Math.floor(num.toString(16) / base)) + (num.toString(16) % base));
-    }
+    return num.toString(base);
   };
 };
