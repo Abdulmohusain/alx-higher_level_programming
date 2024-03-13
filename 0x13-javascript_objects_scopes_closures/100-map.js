383c2fdc63d617c93b4c7e4c3acbbccfc73de35a
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-13 15:56:09 +0100

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const myList = require('./100-data').list;
+const myList = require('./100-data.js').list;
+const newList = myList.map((element, index) => element * index);
 console.log(myList);
-console.log(myList.map((element, index) => element * index));
+console.log(newList);
