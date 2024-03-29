4649f21741104a294fb45f667473ceaa784f5c24
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-29 14:22:08 -0700

    Alx

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """"List of integers"""
+    if list_of_integers:
+        return list_of_integers.sort()[-1]
