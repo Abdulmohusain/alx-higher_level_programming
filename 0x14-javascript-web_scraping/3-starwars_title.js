47e62a7fde9c6bc4d5cbc4b06ac02c671024e53f
Abdullahi Husain <abdulmohusain@gmail.com>
2024-04-23 16:58:01 +0100

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const options = {
+  url: 'https://swapi-api.alx-tools.com/api/films/' + args[2],
+  method: 'GET',
+  json: true
+};
+request(options, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log('Response Body:', body.title);
+});
